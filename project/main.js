e3a4b68e301a515aeaf0e1509698945f742ded12
reinheimermat <mreinheimer0@gmail.com>
2023-10-25 20:16:31 -0300

    javascript-config

--- project/main.js
+++ project/main.js
@@ -1,1 +1,16 @@
+// Import our custom CSS
+import "./src/scss/styles.scss";
 
+const img = document.getElementById("image");
+const inputCheck = document.querySelector("#dark-mode");
+const body = document.querySelector("body");
+
+inputCheck.addEventListener("click", (_) => {
+  const modo = inputCheck.checked ? "dark" : "light";
+  body.setAttribute("data-bs-theme", modo);
+  body.classList.toggle("dark");
+
+  body.classList.contains("dark")
+    ? img.setAttribute("src", "./assets/logo-dark.png")
+    : img.setAttribute("src", "https://github.com/reinheimermat.png");
+});
